bfa0539b7e8c947b9ca7e0cd667d90caaff37fd7
tmayer18 <tmayer18@aol.com>
2022-01-31 02:00:37 -0500

    Homework 4 pipeFlow
    Solves for the flowrate through an elbow. Currently does not agree with hand-calculations!

--- pipeFlow.py
+++ pipeFlow.py
@@ -6,47 +6,72 @@
 # Will compute the pressure [psf] and flowrate [slug/s] at the ends of a pipe
 
 import numpy as np
-from pipe_structures import *
+import pipe_structures as pipes
+import boundary_conditions as bc
+from colebrook_friction_factor import fully_turbulent_f
 
 # %% Inputs
-D = 0.936/12 # [ft] : Pipe Diameter
-ϵ = 2e-4 # [ft] : Pipe Roughness
-L = 6 # [ft] : Pipe Length
-bc = [16*144, 14.7*144] # [psf] pressure at inlet and outlet -> TODO wil be moved to its own handler with more bc support
+NUM_STATES = 2 # number of states per node to solve for
+# Currently pressure, p and mass flowrate, ṁ
 
-my_pipe = Pipe(L, D, 1, 2, ϵ/D, 1)
+pipe_network = (
+    pipes.Minor(1.049/12,  1.049/12, 0, 1, 30*fully_turbulent_f(4.95e-5/(1.049/12))),
+    #pipes.Pipe(6, 0.936/12, 0, 1, 2e-4/(.936/12), 1),
+    bc.BoundaryCondition(0, 1.5*144, "pressure"), 
+    bc.BoundaryCondition(1, 0*144, "pressure")
+)
+
 water = {
     "ρ": 1.94,  # [slugs/ft^3] : Density
     "μ": 2.34e-5, # [lbf-s/ft^2] : Viscosity
     }
 
-max_iterations = 40 # maximum allowable iterations
+max_iterations = 60 # maximum allowable iterations
 desired_tolerance = 1e-8 # desired tolerance in solution. When to cease iteration
 
-# %% Evaluate ans setup piping network
-# TODO comments in the provided matlab code hint that this perhaps will involve combining different pipe elements into a larger M matrix for solving.
+# %% Pre-printing information
+# print some stuff about number of piping elements, and size of M matrix
+N = 0 # number of nodes
+nodes = []
+pipe_likes = 0
+for elem in pipe_network:
+    N += elem.num_nodes
+    if type(elem) in [pipes.Minor, pipes.Pipe]:
+        pipe_likes += 1 # count the number of pipe-likes in the network
 
-# print some stuff about number of piping elements, and size of M matrix (currently 1 element, 4x4 matrix)
+N = int(N/2) # Each node in the network shows up exactly twice in the network, at the start and end of its pipe-like, or as a boundary condition
+# TODO more error-checking on the pipe network to ensure that is is valid
+print(f"There are {pipe_likes} pipe-likes in the network, requiring a {N*2}x{N*2} matrix to solve\n")
+
 
 # %% Iterate on the equations
-p_n = 0.1*np.ones((4,1)) # init column solution vector
+p_n = 0.1*np.ones((N*NUM_STATES,1)) # init column solution vector
 
 err = 10e2 # init error value
 i = 0 # iteration counter
 
 while abs(err) > desired_tolerance and i <= max_iterations:
-    A,b = my_pipe.compute_pipe(p_n, water, bc)
-    # this will get more advanced as we get to non-singular element networks
+    A = np.empty((0, N*NUM_STATES)) # init empty matrix
+    b = np.empty((0,1))
+    
+    for elem in pipe_network:
+        Ai, bi = elem.compute(p_n, water, N)
+        A = np.append(A, Ai, axis=0) # append matrix-linearized equations to the matrix
+        b = np.append(b, bi, axis=0)
+    
+    print(f"CHECK: the length of the matrix is {len(A)}x{len(A[0])}")
 
     p_n1 = np.linalg.solve(A,b) # solve linear equation Ax=b
 
-    err = max(abs( (p_n-p_n1)/(p_n+1e-16) )) # largest change in any solution value
+    err = max(abs( (p_n-p_n1)/(p_n+1e-16) )) # largest percent change in any solution value
+    print((p_n-p_n1)/(p_n+1e-16))
 
     print(f"Solution Vector at iteration {i}: {p_n1}")
     print(f"Error at iteration {i}: {err}")
 
     i+=1
-    p_n = p_n1 # p_n = p_n+1
+    p_n = p_n1.copy() # p_n = p_n+1
+    # copy is necessary cause pointers. Otherwise they will be the same object
 
 print(f"The solution is determined to be {p_n}")
 if i >= max_iterations:

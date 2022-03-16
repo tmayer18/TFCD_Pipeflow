60d2f49281fe1735e3832029aef5ec56c5e9bd9a
tmayer18 <tmayer18@aol.com>
2022-03-16 08:21:11 -0400

    Homework 7
    -looped wind tunnel fan-curve problem

--- pipeFlow.py
+++ pipeFlow.py
@@ -1,9 +1,9 @@
 # MAE 413 - TFCD
-# Homework 6
+# Homework 7
 # Timothy Mayer
-# 2/8/2022
+# 2/14/2022
 
-# Will compute the pressure [psf] and flowrate [slug/s] at the ends of a pipe network
+# Verifies the selection of a fan for a looped segment of pipe
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,38 +16,40 @@ from colebrook_friction_factor import fully_turbulent_f
 NUM_STATES = 2 # number of states per node to solve for
 # Currently pressure, p and mass flowrate, ṁ
 
-D = 4.026/12 # [ft] : pipe diameter
-ϵ = 1.8e-3/12 # [ft] : roughness
-ϵD = ϵ/D # [ul] : relative roughness, ϵ/D
-ft = fully_turbulent_f(ϵD) # [ul] : fully turbulent friction factor
-K_elb = 30*ft
-K_entr = 0.5
-K_filt = 5.5
+D = 3 # [ft] : pipe diameter
+K_elb = .30 # Loss coefficient for smooth elbow
 
 water = {
     "ρ": 1.94,  # [slugs/ft^3] : Density
     "μ": 2.34e-5, # [lbf-s/ft^2] : Viscosity
     }
 
-goulds_curve_head = np.array([108.936, 108.936, 108.085, 107.66, 106.383, 104.255, 100.851, 97.0213, 93.1915, 88.0851, 81.7021, 75.3191, 69.3617, 65.9574, 0])
-goulds_curve_flowrate = np.array([0, 100.127, 200.413, 279.361, 375.378, 462.859, 565.274, 665.555, 748.766, 827.707, 919.45, 996.256, 1064.53, 1098.66, 1446.37])
-Goulds3196 = pumps.PumpCurve(goulds_curve_flowrate, goulds_curve_head, fluid=water, units='QH')
+air = {
+    "ρ": 0.075/32.174, # [slug/ft^3] : Density
+    "μ": 3.74e-7, #[lbf-s/ft^2] : Viscosity
+}
+
+vad_fan_head = np.array([4.91439,4.31408,3.79041,3.39776,2.96155,2.52539,2.04565,1.62047]) # [in wg]   ##TODO y crossing at 6.4269,
+vad_fan_flowrate = np.array([38034.6,45148.7,50162.8,53427.5,56691.8,59722.7,62870.1,65434.3]) # cfm
+
+vad_fan_head *= 5.20 # [in wg] -> [psf]
+vad_fan_flowrate *= air["ρ"]/60 # [cfm] -> [slug/s]
+
+VAD48H21 = pumps.PumpCurve(vad_fan_flowrate, vad_fan_head, fluid=air, units='mdp')
 
 pipe_network = (
-    bc.BoundaryCondition(0, 0, "pressure"), # p_atm
-    pipes.Pipe(5.3, 4, 0,1, ϵD, -5.3), # full tank
-    # pipes.Pipe(0, 4, 0,1, ϵD, 0), # empty tank
-    pipes.Minor(D,D, 1,2, K_entr),
-    pipes.Pipe(10, D, 2,3, ϵD, 0),
-    pumps.Pump(3,4, Goulds3196),
-    pipes.Pipe(20, D, 4,5, ϵD, 0),
-    pipes.Minor(D,D, 5,6, K_filt),
-    pipes.Pipe(20, D, 6,7, ϵD, 0),
+    bc.BoundaryCondition(0,0,"pressure"), # reference 0 point, after fan
+    pipes.Pipe(20, D, 0,1, 0, 0),
+    pipes.Minor(D,D, 1,2, K_elb),
+    pipes.Pipe(20, D, 2,3, 0, 0),
+    pipes.Minor(D,D, 3,4, K_elb),
+    pipes.Pipe(40, D, 4,5, 0, 0),
+    pipes.Minor(D,D, 5,6, K_elb),
+    pipes.Pipe(20, D, 6,7, 0, 0),
     pipes.Minor(D,D, 7,8, K_elb),
-    pipes.Pipe(6, D, 8,9, ϵD, 6),
-    pipes.Minor(D,D, 9,10, K_elb),
-    pipes.Pipe(15, D, 10,11, ϵD, 0),
-    bc.BoundaryCondition(11, 0, "pressure") # p_atm
+    pipes.Pipe(20, D, 8,9, 0, 0),
+    pumps.Pump(9,10, VAD48H21),
+    bc.BoundaryCondition(10,0,"pressure")
 )
 
 max_iterations = 40 # maximum allowable iterations
@@ -71,7 +73,7 @@ print(f"There are {pipe_likes} pipe-likes in the network, requiring a {N*2}x{N*2
 
 
 # %% Iterate on the equations
-p_n = 0.1*np.ones((N*NUM_STATES,1)) # init column solution vector
+p_n = 10*np.ones((N*NUM_STATES,1)) # init column solution vector
 
 err = 10e2 # init error value
 i = 0 # iteration counter
@@ -81,7 +83,7 @@ while abs(err) > desired_tolerance and i <= max_iterations:
     b = np.empty((0,1))
     
     for elem in pipe_network:
-        Ai, bi = elem.compute(p_n, water, N)
+        Ai, bi = elem.compute(p_n, air, N)
         A = np.append(A, Ai, axis=0) # append matrix-linearized equations to the matrix
         b = np.append(b, bi, axis=0)
     
@@ -104,17 +106,17 @@ if i >= max_iterations:
 
 print(" SOLUTION ")
 print("Node     ṁ          Q         p           ρ          μ")
-print("      [slug/s]    [gpm]     [psig]    [slug/cf]  [lbf*s/ft^2]")
+print("      [slug/s]    [cfm]     [psig]    [slug/cf]  [lbf*s/ft^2]")
 for i in range(N):
-    print(f"{i}    {float(p_n[i+N]):0.7f}    {float(p_n[i+N]/water['ρ']/0.13368*60):0.2f}      {float(p_n[i]/144):4.2f}      {water['ρ']}      {water['μ']}")
+    print(f"{i}    {float(p_n[i+N]):0.7f}    {float(p_n[i+N]/air['ρ']*60):0.2f}      {float(p_n[i]/144):4.2f}      {air['ρ']:4.4f}      {air['μ']}")
 
 # %plotting results
 fig, ax = plt.subplots(figsize=(8.5,5))
-Goulds3196.plot()
-plt.plot(p_n[3+N], p_n[4]-p_n[3], '.r')
-plt.title("Full Tank Case")
+VAD48H21.plot()
+plt.plot(p_n[9+N], p_n[10]-p_n[9], '.r')
+plt.title("Operational Point of VAD48H21 Fan in Wind Tunnel")
 plt.legend(("Pumpcurve", "Solution"))
-plt.ylabel("Pressure Rise over Pump [psf]")
+plt.ylabel("Pressure Rise over Fan [psf]")
 plt.xlabel("Flowrate [slug/s]")
 plt.grid()
 plt.show()

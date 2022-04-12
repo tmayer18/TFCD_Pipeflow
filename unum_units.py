a7c115cf817450000bb040db5f1b2e8f3c144bc2
tmayer18 <tmayer18@aol.com>
2022-04-11 21:15:26 -0400

    Unum2 base_unit_convert and Unum1 unit port
    - to_base_units recursive method completion
    - dynamic update of library provided defined to the Unum2 format

--- unum_units.py
+++ unum_units.py
@@ -49,7 +49,7 @@ class Unum2(Unum):
                 target_unit *= Unum2._base_unit_table[unit_key]**unit_pow
             else:
                 # otherwise recursively retrieve lineage of units, starting with the next findable in the unit table
-                parent_unit = Unum2(Unum.getUnitTable()[unit_key][0]._unit)**unit_pow
+                parent_unit = Unum2(Unum2.getUnitTable()[unit_key][0]._unit)**unit_pow
                 target_unit *= parent_unit.as_base_unit() # if rec_expand is a base unit, recursive call will return target_unit=self
         
         target_unit._value = 1 # overwrite conversion factor, as_unit will handle the conversion factors
@@ -58,12 +58,11 @@ class Unum2(Unum):
     # now we wrap the methods of Unum1 that explicitly return a Unum1 object to now return Unum2 objects
     _upgrade_methods = ['__add__', '__sub__', '__mul__', '__div__',
         '__floordiv__', '__pow__', '__abs__', '__radd__', '__rsub__', '__rmul__',
-        '__rdiv__', '__rfloordiv__', '__rpow__', '__getitem__', '__setitem__']
-    #TODO __pos__ and __neg__ are missing since they are single-argument
+        '__rdiv__', '__rfloordiv__', '__rpow__', '__getitem__', '__setitem__',
+        '__pos__', '__neg__']
     for method_str in _upgrade_methods:
-        def wrapped_method(self, other=None, method_str=method_str):
-            ret_obj = getattr(super(), method_str)(other)
-            #print(f"{method_str}: Re-returning Unum2({ret_obj._unit}, {ret_obj._value})")
+        def wrapped_method(self, *args, method_str=method_str):
+            ret_obj = getattr(super(), method_str)(*args)
             return Unum2(ret_obj._unit, ret_obj._value)
         vars()[method_str] = wrapped_method
 
@@ -77,20 +76,23 @@ class Unum2(Unum):
 class units2():
     '''upconvert all predefined Unum1 units to Unum2, but as a class instead of the
     import bullcrap Unum1 did'''
-    exceptions = {"deg C":"celsius", "'":"arcmin", "''":"arcsec", "nmi":"nmile"}
-    ignores = ["u"]
+    # note, that since we're dynamically generating these attributes, the linter will fail to show them properly, so we'll manually set their names here to avoid
+    # errors in the linting process
+    m, Ym, Zm, Em, Pm, Tm, Gm, Mm, km, hm, dam, ym, zm, am, fm, pm, nm, um, mm, cm, dm, s, Ys, Zs, Es, Ps, Ts, Gs, Ms, ks, hs, das, ys, zs, fs, ps, ns, us, ms, cs, ds, A, YA, ZA, EA, PA, TA, GA, MA, kA, hA, daA, yA, zA, aA, fA, pA, nA, uA, mA, cA, dA, K, YK, ZK, EK, PK, TK, GK, MK, kK, hK, daK, yK, zK, aK, fK, pK, nK, uK, mK, cK, dK, mol, Ymol, Zmol, Emol, Pmol, Tmol, Gmol, Mmol, kmol, hmol, damol, ymol, zmol, amol, fmol, pmol, nmol, umol, mmol, cmol, dmol, cd, Ycd, Zcd, Ecd, Pcd, Tcd, Gcd, Mcd, kcd, hcd, dacd, ycd, zcd, acd, fcd, pcd, ncd, ucd, mcd, ccd, dcd, kg, Yg, Zg, Eg, Pg, Tg, Gg, Mg, hg, dag, yg, zg, ag, fg, pg, ng, ug, mg, cg, dg, g, rad, sr, Hz, N, Pa, J, W, C, V, F, ohm, S, Wb, T, H, celsius, lm, lx, Bq, Gy, Sv, kat, min, h, d, deg, L, t, Np, dB, eV, ua, mile, nmile, knot, a, ha, bar, angstrom, b, Ci, R, rem = [None]*189
+    
+    exceptions = {"deg C":"celsius", "nmi":"nmile"}
+    ignores = ["u", "'", "''"]
     unum1_table = Unum.getUnitTable()
     Unum.reset() # now reset the base class table so we can reregister as Unum2
-    for symbol, unit_val in unum1_table.items():
-        # print(f"Recreating {symbol}, {unit_val}")
-        # if symbol in exceptions:
-        #     symbol = exceptions[symbol] # handle naming inconsistencies
+    for symbol, (conv, _, name) in unum1_table.items():
+        if conv is None:
+            conv = 0 # for some reason, 0 indicates base unit, not None
+        if symbol in exceptions:
+            symbol = exceptions[symbol] # handle naming inconsistencies
         if symbol not in ignores:
-            # print(f"unit({symbol}, {unit_val[0]}, {unit_val[2]})")
-            try:
-                vars()[symbol] = Unum2.unit(symbol, unit_val[0], unit_val[2])
-            except:
-                pass
+            # print(f"{symbol}", end=', ') # print out full list of units for above line
+            vars()[symbol] = Unum2.unit(symbol, conv, name)
+
 
 if __name__ == "__main__":
     print("\n====Main====\n")
@@ -105,21 +107,20 @@ if __name__ == "__main__":
     hhlTeic = Unum2.unit('hhlTeic', 300*hlTeic, 'hip half lil teichert')
     Can = Unum2.unit('can', 0, "canino")
     gen = Unum2.unit('gen', 0.5*lTeic*1.2*Can, 'genius')
+
+    print(u)
+    print(units2)
+    print(units2.celsius)
     
-    #print(a)
-    a = 2*hhlTeic**2 * 4*Can
-    a2 = 2*hhlTeic * 4*Can
-    b = 8*gen
-    c = 1*hlTeic**2
-    d = 1*hhlTeic
-    print([a])
-    from pprint import pprint
-    pprint(Unum2.getUnitTable())
-    # print(b.asUnit(Teic*Can))
-    # print(c.as_base_unit())
-    # print(d.as_base_unit())
-    print(a.as_base_unit())
-    # print(a2.as_base_unit())
+    a = hhlTeic * Can * units2.mm
+    b = units2.N
+    c = -b
+    d = +c
+    e = a*b
+    print(a)
+    print(b.as_base_unit())
+    print(c, d, e)
+
     
     
 
